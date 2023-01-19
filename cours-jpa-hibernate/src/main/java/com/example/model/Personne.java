6b858f537a88f852a818ea611cf8929805e82dec
youssef baddi <baddiyoussef85@gmail.com>
2023-01-19 14:52:39 +0100

    add config file as a class

--- cours-jpa-hibernate/src/main/java/com/example/model/Personne.java
+++ cours-jpa-hibernate/src/main/java/com/example/model/Personne.java
@@ -1,7 +1,15 @@
 package com.example.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Personne {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int num;
 	private String nom;
 	private String prenom;
