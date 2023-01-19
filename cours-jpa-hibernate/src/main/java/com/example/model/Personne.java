b177e101b6f3ab16043534262f64729a1ba588e5
youssef baddi <baddiyoussef85@gmail.com>
2023-01-19 14:45:38 +0100

    add config file

--- cours-jpa-hibernate/src/main/java/com/example/model/Personne.java
+++ cours-jpa-hibernate/src/main/java/com/example/model/Personne.java
@@ -2,4 +2,41 @@ package com.example.model;
 
 public class Personne {
 
+	private int num;
+	private String nom;
+	private String prenom;
+	public Personne() {
+//		super();
+		//TODO Auto-generated constructor stub
+	}
+	public Personne(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	@Override
+	public String toString() {
+		return "Personne [num=" + num + ", nom=" + nom + ", prenom=" + prenom + "]";
+	}
+	
+	
+	
 }
